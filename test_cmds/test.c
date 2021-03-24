5bafd6776ce9abb834df9527ae785d071ef2e87e
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2021-03-24 15:35:30 +0100

    modify [",\,']

--- test_cmds/test.c
+++ test_cmds/test.c
@@ -6,7 +6,7 @@
 /*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 18:15:05 by abdel-ke          #+#    #+#             */
-/*   Updated: 2021/03/24 15:22:08 by abdel-ke         ###   ########.fr       */
+/*   Updated: 2021/03/24 15:33:50 by abdel-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,11 +80,11 @@ void	check_quote(char *line)
 int main()
 {
 	char	*line;
-
+	char *minishel = "\n\e[1;32m$minishel\033[1;34m=> \033[0m";
 	line = NULL;
 	while(1337)
 	{
-		write(1,"\n\e[1;32m$minishel\033[1;34m=> \033[0m",ft_strlen("\n\e[1;32m$minishel\033[1;34m=> \033[0m"));
+		write(1, minishel,ft_strlen(minishel));
 		get_next_line(&line);
 		check_quote(line);
 		free(line);
